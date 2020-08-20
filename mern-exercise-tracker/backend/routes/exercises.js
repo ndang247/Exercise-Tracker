5b261b3b6032f2c400cdcc52d42d3c0b462a8083
Nam-Dang13581234 <nathandang73@gmail.com>
2020-08-20 23:49:21 +1000

    Complete backend

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // call the router
 const router = express.Router();
-// require the user model this is for instantiating newUser at line 20
+// require the user model this is for instantiating newUser at line 24
 const Exercise = require('../models/exercise.model');
 
 // if the user goes to /exercises/ => get request
@@ -33,6 +33,42 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('Exercise added!')) // then return Exercise added! in json
     .catch(err => res.json('Error: ' + err)); // or else return an err msg
 });
+
+// GET request a json object from the database using id
+// then only return the information of that object id
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+// DELETE request a json object from the database using id
+// then only delete that object
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Excersie deleted.'))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+// UPDATE request a json object from the database using id
+// then only update that object
+router.route('/update/:id').post((req, res) => {
+    // find the current exercise then update it
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+        
+        exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error ' + err));
+    })
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+
 
 // exporting the router
 module.exports = router;
